1015583122db700a39847a3f6ee7473e9a7f0ca7
Trần Kiên Cường <trankiencuong30072003@gmail.com>
2024-11-14 15:16:54 +0700

    Chapter5: Learn about strings in C++

---
+++ Chapter5/FormatString.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() { 
+    string str;
+
+    cout << "Nhap chuoi can format: ";
+    getline(cin, str);
+
+    while(str[0] == ' ') str.erase(0, 1);
+
+    while(str[str.length() -1] == ' ') str.erase(str.length() -1);
+
+    int i = 0;
+    while(i < str.length()) 
+        if((str[i] == ' ') && (str[i+1] == ' ')) str.erase(i, 1);
+        else i++;
+    
+    for(int i = 0; i < str.length(); i++) str[i] = tolower(str[i]);
+    
+    str[0] = toupper(str[0]);
+    for(int i = 0; i < str.length(); i++) 
+        if((str[i] == ' ') && (str[i+1] != ' ')) str[i+1] = toupper(str[i+1]);
+
+    cout << "Chuoi sau khi format: " << str << endl;
+
+    return 0;
+}
